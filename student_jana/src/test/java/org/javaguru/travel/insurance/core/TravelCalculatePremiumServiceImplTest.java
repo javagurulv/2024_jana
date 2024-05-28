af6519c200da3e7bb3f62fbc9caa3ad884c33231
janase1 <yana.sergeyenko@gmail.com>
2024-05-28 15:04:18 +0300

    step_14_description.txt added step_14 done

--- student_jana/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
+++ student_jana/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
@@ -9,34 +9,48 @@ import java.util.Date;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TravelCalculatePremiumServiceImplTest {
+    private TravelCalculatePremiumService service = new TravelCalculatePremiumServiceImpl();
 
     @Test
-    public void testCalculatePremium() {
-        TravelCalculatePremiumService service = new TravelCalculatePremiumServiceImpl();
+    public void testPersonFirstName() {
         TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
         request.setPersonFirstName("Name");
-        request.setPersonLastName("Surname");
-        request.setAgreementDateTo(new Date());
-        request.setAgreementDateFrom(new Date());
-
-        TravelCalculatePremiumResponse response = service.calculatePremium(request);
+        TravelCalculatePremiumResponse response =service.calculatePremium(request);
         assertEquals(response.getPersonFirstName(),request.getPersonFirstName());
-        assertEquals(response.getPersonLastName(),request.getPersonLastName());
-        assertEquals(response.getAgreementDateTo(),request.getAgreementDateTo());
-        assertEquals(response.getAgreementDateFrom(),request.getAgreementDateFrom());
-
+        System.out.println("First name test finished");
         System.out.println(response.getPersonFirstName());
         System.out.println(request.getPersonFirstName());
 
+    }
+    @Test
+    public void testPersonLastName() {
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
+        request.setPersonLastName("Surname");
+        TravelCalculatePremiumResponse response =service.calculatePremium(request);
+        assertEquals(response.getPersonLastName(),request.getPersonLastName());
+        System.out.println("Last name test finished");
         System.out.println(response.getPersonLastName());
         System.out.println(request.getPersonLastName());
-
+    }
+    @Test
+    public void testgetAgreementDateTo() {
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
+        request.setAgreementDateTo(new Date());
+        TravelCalculatePremiumResponse response =service.calculatePremium(request);
+        assertEquals(response.getAgreementDateTo(),request.getAgreementDateTo());
+        System.out.println("Date To test finished");
         System.out.println(response.getAgreementDateTo());
         System.out.println(request.getAgreementDateTo());
-
+    }
+    @Test
+    public void testgetAgreementDateFrom() {
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
+        request.setAgreementDateFrom(new Date());
+        TravelCalculatePremiumResponse response =service.calculatePremium(request);
+        assertEquals(response.getAgreementDateFrom(),request.getAgreementDateFrom());
+        System.out.println("Date From test finished");
         System.out.println(response.getAgreementDateFrom());
         System.out.println(request.getAgreementDateFrom());
-
     }
 
 }
