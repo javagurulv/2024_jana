e3c2d179db4b4f7a5538b36ecbf469bf8bfc3d65
janase1 <yana.sergeyenko@gmail.com>
2024-05-29 13:20:14 +0300

    step_15_description.txt added step_15 fix done

--- student_jana/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplAIOneTest.java
+++ student_jana/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplAIOneTest.java
@@ -3,13 +3,16 @@ package org.javaguru.travel.insurance.core;
 import org.javaguru.travel.insurance.rest.TravelCalculatePremiumRequest;
 import org.javaguru.travel.insurance.rest.TravelCalculatePremiumResponse;
 import org.junit.jupiter.api.Test;
+
 import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TravelCalculatePremiumServiceImplAIOneTest {
 
     @Test
-    public void testCalculatePremium_WithValidRequest_ShouldSetResponseProperties() {
+    public void testCalculatePremium_WithValidRequest_ShouldSetAllResponseProperties() {
         // Создаем сервис и запрос
         TravelCalculatePremiumService service = new TravelCalculatePremiumServiceImpl();
         TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
@@ -24,24 +27,20 @@ public class TravelCalculatePremiumServiceImplAIOneTest {
         TravelCalculatePremiumResponse response = service.calculatePremium(request);
 
         // Проверяем, что свойства объекта response установлены правильно
-        assertEquals(request.getPersonFirstName(), response.getPersonFirstName());
-        assertEquals(request.getPersonLastName(), response.getPersonLastName());
-        assertEquals(request.getAgreementDateFrom(), response.getAgreementDateFrom());
-        assertEquals(request.getAgreementDateTo(), response.getAgreementDateTo());
+        assertEquals("John", response.getPersonFirstName());
+        assertEquals("Doe", response.getPersonLastName());
+        assertEquals(dateFrom, response.getAgreementDateFrom());
+        assertEquals(dateTo, response.getAgreementDateTo());
     }
 
     @Test
-    public void testCalculatePremium_WithNullRequest_ShouldReturnResponseWithNullProperties() {
+    public void testCalculatePremium_WithNullRequest_ShouldThrowNullPointerException() {
         // Создаем сервис
         TravelCalculatePremiumService service = new TravelCalculatePremiumServiceImpl();
 
-        // Вызываем метод calculatePremium с null в качестве аргумента
-        TravelCalculatePremiumResponse response = service.calculatePremium(null);
-
-        // Проверяем, что свойства объекта response равны null
-        assertEquals(null, response.getPersonFirstName());
-        assertEquals(null, response.getPersonLastName());
-        assertEquals(null, response.getAgreementDateFrom());
-        assertEquals(null, response.getAgreementDateTo());
+        // Проверяем, что вызывая метод calculatePremium с null, выбрасывается NullPointerException
+        assertThrows(NullPointerException.class, () -> {
+            service.calculatePremium(null);
+        });
     }
 }
