1c9333439a02c07aa394aeade8f71a75d46a4175
janase1 <yana.sergeyenko@gmail.com>
2024-05-29 13:37:22 +0300

    step_16_description.txt added step_16 done

--- student_jana/src/main/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumResponse.java
+++ student_jana/src/main/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumResponse.java
@@ -1,5 +1,6 @@
 package org.javaguru.travel.insurance.rest;
 
+import java.math.BigDecimal;
 import java.util.Date;
 
 public class TravelCalculatePremiumResponse {
@@ -8,16 +9,19 @@ public class TravelCalculatePremiumResponse {
     private String personLastName;
     private Date agreementDateFrom;
     private Date agreementDateTo;
+    private BigDecimal agreementPrice;
 
     public TravelCalculatePremiumResponse() {}
     public TravelCalculatePremiumResponse(String personFirstName,
                                          String personLastName,
                                          Date agreementDateFrom,
-                                         Date agreementDateTo) {
+                                         Date agreementDateTo,
+                                          BigDecimal agreementPrice) {
         this.personFirstName = personFirstName;
         this.personLastName = personLastName;
         this.agreementDateFrom = agreementDateFrom;
         this.agreementDateTo = agreementDateTo;
+        this.agreementPrice = agreementPrice;
     }
 
     public String getPersonFirstName() {
@@ -51,5 +55,9 @@ public class TravelCalculatePremiumResponse {
     public void setAgreementDateTo(Date agreementDateTo) {
         this.agreementDateTo = agreementDateTo;
     }
+
+    public BigDecimal getAgreementPrice() {return agreementPrice;}
+
+    public void setAgreementPrice(BigDecimal agreementPrice) {this.agreementPrice = agreementPrice;}
 
 }
