80a9267b2c1dc59273fd5147c4bd0a8d12236246
janase1 <yana.sergeyenko@gmail.com>
2024-06-01 11:05:12 +0300

    step_18_description.txt added step_18 done

--- student_jana/src/main/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
+++ student_jana/src/main/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
@@ -17,15 +17,26 @@ class TravelCalculatePremiumServiceImpl implements TravelCalculatePremiumService
         TravelCalculatePremiumResponse response = new TravelCalculatePremiumResponse();
         response.setPersonFirstName(request.getPersonFirstName());
         response.setPersonLastName(request.getPersonLastName());
-        LocalDate agreementDateFrom = LocalDate.of(2024, 5, 24);
-        LocalDate agreementDateTo = LocalDate.of(2024, 5, 27);
-        Date fromDate = Date.from(agreementDateFrom.atStartOfDay(ZoneId.systemDefault()).toInstant());
-        Date toDate = Date.from(agreementDateTo.atStartOfDay(ZoneId.systemDefault()).toInstant());
-        response.setAgreementDateFrom(fromDate);
-        response.setAgreementDateTo(toDate);
-        long diff=request.getAgreementDateTo().getTime() - request.getAgreementDateFrom().getTime();
-        var daysBetween = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
-        response.setAgreementPrice(new BigDecimal(daysBetween));
+        Date agreementDateFrom = calculateAgreementDateFrom();
+        Date agreementDateTo = calculateAgreementDateTo();
+        response.setAgreementDateFrom(agreementDateFrom);
+        response.setAgreementDateTo(agreementDateTo);
+        BigDecimal daysBetween=calculateDaysBetweenAgreementDateToAndAgreementDateFrom();
+        response.setAgreementPrice(new BigDecimal(String.valueOf(daysBetween)));
         return response;
+    }
+
+        private Date calculateAgreementDateFrom(){
+            LocalDate agreementDateFrom = LocalDate.of(2024, 5, 24);
+            return Date.from(agreementDateFrom.atStartOfDay(ZoneId.systemDefault()).toInstant());
+        }
+        private Date calculateAgreementDateTo(){
+            LocalDate agreementDateTo = LocalDate.of(2024, 5, 27);
+            return Date.from(agreementDateTo.atStartOfDay(ZoneId.systemDefault()).toInstant());
+        }
+    private BigDecimal calculateDaysBetweenAgreementDateToAndAgreementDateFrom(){
+        long diff = calculateAgreementDateTo().getTime() - calculateAgreementDateFrom().getTime();
+        var daysBetween = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+        return (new BigDecimal(daysBetween));
     }
 }
