f62f98ced9ac2bd0606f84c97bf1a676e7b1a582
janase1 <yana.sergeyenko@gmail.com>
2024-06-09 06:05:38 +0300

    step 29 done

--- student_jana/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumRequestValidatorLastNameTest.java
+++ student_jana/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumRequestValidatorLastNameTest.java
@@ -3,6 +3,7 @@ package org.javaguru.travel.insurance.core;
 import org.javaguru.travel.insurance.rest.TravelCalculatePremiumRequest;
 import org.javaguru.travel.insurance.rest.ValidationError;
 import org.junit.jupiter.api.Test;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -11,28 +12,31 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TravelCalculatePremiumRequestValidatorTest {
+public class TravelCalculatePremiumRequestValidatorLastNameTest {
     private TravelCalculatePremiumRequestValidator validator= new TravelCalculatePremiumRequestValidator();
     @Test
-    public void testFirstNameIsEmpty() throws ParseException {
+    public void testLastNameIsEmpty() throws ParseException {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         Date agreementDateFrom = dateFormat.parse("2024-05-24");
         Date agreementDateTo = dateFormat.parse("2024-05-27");
-        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest("","surname", agreementDateFrom,agreementDateTo);
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest("name","", agreementDateFrom,agreementDateTo);
         List<ValidationError> result=validator.validate(request);
         List<ValidationError> expected = new ArrayList<>();
-        expected.add(new ValidationError("personFirstName", "Must not be empty!"));
+        expected.add(new ValidationError("personLastName", "Must not be empty!"));
         assertEquals(expected.toString(),result.toString());
+        System.out.println("Result: " + result.toString());
+        System.out.println("Expected: " + expected.toString());
 
     }
     @Test
-    public void testFirstNameIsNotEmpty() throws ParseException {
+    public void testLastNameIsNotEmpty() throws ParseException {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         Date agreementDateFrom = dateFormat.parse("2024-05-24");
         Date agreementDateTo = dateFormat.parse("2024-05-27");
         TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest("name","surname", agreementDateFrom,agreementDateTo);
         List<ValidationError> result=validator.validate(request);
         List<ValidationError> expected = new ArrayList<>();
+        System.out.println("Result: " + result.toString());
+        System.out.println("Expected: " + expected.toString());
     }
-
 }
