215c117bd8ccd5fc63d21b4fa476939632845f09
janase1 <yana.sergeyenko@gmail.com>
2024-06-21 11:28:50 +0300

    step 38

--- student_jana/src/main/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumController.java
+++ student_jana/src/main/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumController.java
@@ -3,6 +3,7 @@ package org.javaguru.travel.insurance.rest;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.javaguru.travel.insurance.core.TravelCalculatePremiumService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/insurance/travel")
-@RequiredArgsConstructor(access = AccessLevel.PACKAGE)
+//@RequiredArgsConstructor(access = AccessLevel.PACKAGE)
 
 public class TravelCalculatePremiumController {
 
 	private final TravelCalculatePremiumService calculatePremiumService;
+	@Autowired
+
+	TravelCalculatePremiumController(TravelCalculatePremiumService calculatePremiumService) {
+		this.calculatePremiumService = calculatePremiumService;
+	}
 
 	@PostMapping(path = "/",
 			consumes = "application/json",
